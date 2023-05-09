c0dd6ac96390c82de00a6fb9ac65977c2c4f8577
iamando <iamando@tuta.io>
2023-05-09 12:02:00 +0300

    feature: re-fact test case

--- logaro_test.go
+++ logaro_test.go
@@ -44,39 +44,3 @@ func TestGenerateLogger(t *testing.T) {
 		t.Errorf("Log entry mismatch.\nExpected: %+v\nCaptured: %+v", expectedEntry, capturedEntry)
 	}
 }
-
-func compareLogEntries(a, b LogEntry) bool {
-	// Compare Timestamp, Message, Level, and Fields
-	return a.Timestamp == b.Timestamp &&
-		a.Message == b.Message &&
-		a.Level == b.Level &&
-		compareFields(a.Fields, b.Fields)
-}
-
-func compareFields(a, b map[string]interface{}) bool {
-	// Compare the lengths of the fields maps
-	if len(a) != len(b) {
-		return false
-	}
-
-	// Compare each key-value pair in the fields maps
-	for key, valA := range a {
-		valB, ok := b[key]
-		if !ok || !compareFieldValues(valA, valB) {
-			return false
-		}
-	}
-
-	return true
-}
-
-func compareFieldValues(a, b interface{}) bool {
-	// Marshal and compare the JSON representations of the field values
-	bytesA, errA := json.Marshal(a)
-	bytesB, errB := json.Marshal(b)
-	if errA != nil || errB != nil {
-		return false
-	}
-
-	return bytes.Equal(bytesA, bytesB)
-}
