1a8d3df8501832d53f1f0644ae4eb944aba4bc55
iamando <iamando@tuta.io>
2023-05-09 11:47:41 +0300

    feature: set func comment serialize entry

--- logaro.go
+++ logaro.go
@@ -155,6 +155,13 @@ func (l *Logger) mergeFields(fields map[string]interface{}) map[string]interface
 	return mergedFields
 }
 
+// serializeEntry applies the logger's serializer function to the log entry.
+// If a serializer is set for the logger, it applies the serializer function to the log message
+// and the fields of the entry, allowing custom modification or formatting of the log entry.
+// Returns the serialized log entry with the log message and fields modified by the serializer,
+// ensuring that the log data is transformed according to the specified serialization logic.
+// The function is used to customize the serialization process for specific log entries
+// based on the serializer function set for the logger.
 func (l *Logger) serializeEntry(entry LogEntry) LogEntry {
 	if l.Serializer != nil {
 		entry.Message = l.Serializer(entry.Message).(string)
